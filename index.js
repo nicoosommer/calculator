a86e2ea9253cab06bd69c6a1baac215dac7bf716
nicoosommer <nicosomer388@gmail.com>
2023-09-11 20:30:00 -0300

    Bastantes funcionalidades extras hechas. Faltan las principales

--- index.js
+++ index.js
@@ -21,3 +21,35 @@ function operate(a, b, op) {
   if (op == "*") return multiply(a, b);
   if (op == "/") return divide(a, b);
 }
+
+const display = document.querySelector(".display");
+const nums = document.querySelectorAll(".nmbs");
+const ac = document.querySelector("#ac");
+const punto = document.querySelector(".punto");
+const masmenos = document.querySelector("#masmenos");
+const borrar = document.querySelector(".borrar");
+
+for (num of nums) {
+  num.addEventListener("click", (e) => {
+    display.textContent += e.target.innerText;
+  });
+}
+ac.addEventListener("click", () => {
+  display.textContent = "";
+});
+punto.addEventListener("click", (e) => {
+  const arr = display.textContent.split("");
+  if (arr.includes(".") === true) return;
+  display.textContent += e.target.innerText;
+});
+borrar.addEventListener("click", () => {
+  display.textContent = display.textContent.slice(0, -1);
+});
+
+masmenos.addEventListener("click", () => {
+  if (display.textContent[0] !== "-") {
+    display.textContent = "-" + display.textContent;
+  } else {
+    display.textContent = display.textContent.slice(1);
+  }
+});
